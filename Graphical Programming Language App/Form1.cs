c23d535d60e0518657e3f0928d3b18691d378a52
Victoria <victoriamoles@gmail.com>
2018-11-26 18:50:56 +0000

    Add project files.

---
+++ Graphical Programming Language App/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphical_Programming_Language_App
+{
+    public partial class Form1 : Form, IDraw
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    
+    }
+}
---
+++ Graphical Programming Language App/IDraw.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphical_Programming_Language_App
+{
+    interface IDraw
+    {
+        void moveTo();
+
+        void drawLine();
+
+        void drawRectangle();
+
+        void drawCircle();
+
+        void drawTriangle();
+
+        void drawPolygon();
+    }
+}
